7f6e85f44b7b73521a1183f61cb7f0d5d28a0ddf
cblack90 <46687746+cblack90@users.noreply.github.com>
2020-06-14 14:29:10 -0600

    setup

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,11 +5,22 @@ window._ = require('lodash');
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
+try{
+    window.$ = window.jQuery = require('jquery');
+    
+}catch(e) {}
 
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+let toke = document.head.querySelector('meta[name="csrf-toke"]');
+
+if (token){
+    windows.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}else{
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token;');
+}
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
